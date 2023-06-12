45e1784b7f7e2054421a7de0e5c6ca263fbccee2
TheTFoster <58485707+TheTFoster@users.noreply.github.com>
2023-06-12 11:49:23 -0500

    6/12 Changes

--- FileMerge.py
+++ FileMerge.py
@@ -26,28 +26,25 @@ try:
     df2 = pd.read_excel(payRoll_File)
 
     # Keep only 'Employee name', 'Ticket Date' and 'Agency' columns in df2
-    df2 = df2[['Employee Name', 'Ticket Date', 'Agency', 'Supervisors Name', 'PM Assigned', 'JobNo|Customer|Description']]
+    df2 = df2[
+        ['Employee Name', 'Employee ID', 'Ticket Date', 'Agency', 'Clock-In ID', 'Supervisors Name', 'PM Assigned',
+         'JobNo|Customer|Description', 'Email', 'WTL Approved', 'WTL Start Date', 'WTL End Date']]
 
     # Convert 'Ticket Date' to datetime in both dataframes
     df1['Ticket Date'] = pd.to_datetime(df1['Ticket Date'])
     df2['Ticket Date'] = pd.to_datetime(df2['Ticket Date'])
 
-    # If 'Actual Clock In Time' and 'Actual Clock Out Time' are not datetime, convert them
-    df1['Actual Clock In Time'] = pd.to_datetime(df1['Actual Clock In Time'])
-    df1['Actual Clock Out Time'] = pd.to_datetime(df1['Actual Clock Out Time'])
+    # If 'Clock In' and 'Clock Out' are not datetime, convert them
+    df1['Clock In'] = pd.to_datetime(df1['Clock In'])
+    df1['Clock Out'] = pd.to_datetime(df1['Clock Out'])
 
-    # Set empty 'Actual Clock Out Time' to 'Actual Clock In Time'
-    df1.loc[df1['Actual Clock Out Time'].isnull(), 'Actual Clock Out Time'] = df1['Actual Clock In Time']
+    # Calculate 'Actual Hours Worked' as the difference between 'Clock Out' and 'Clock In', converted to hours
+    df1['Actual Hours Worked'] = (df1['Clock Out'] - df1['Clock In']).dt.total_seconds() / 3600
+    # Taking off the half hour for lunch
+    df1['Actual Hours Worked'] = (df1['Actual Hours Worked'] - .5)
 
-    # Calculate 'Actual Hours Worked' as the difference between 'Actual Clock Out Time' and 'Actual Clock In Time',
-    # converted to hours
-    df1['Actual Hours Worked'] = (df1['Actual Clock Out Time'] - df1['Actual Clock In Time']).dt.total_seconds() / 3600
-
-    # Round 'Actual Hours Worked' to 2 decimal places
-    df1['Actual Hours Worked'] = df1['Actual Hours Worked'].apply(lambda x: round(x, 2))
-
-    # # Round 'Actual Hours Worked' using the 7-minute rule
-    # df1['Actual Hours Worked'] = df1['Actual Hours Worked'].apply(lambda x: round((x * 60 + 7) // 15 / 4, 2))
+    # Calculate overtime hours if 'Actual Hours Worked' is greater than 8
+    df1['Overtime'] = np.where(df1['Actual Hours Worked'] > 8, df1['Actual Hours Worked'] - 8, 0)
 
     # Add 'Day of the Week' column
     df1['Day of the Week'] = df1['Ticket Date'].dt.day_name()
@@ -55,22 +52,25 @@ try:
     # Merge dataframes based on 'Employee name' and 'Ticket Date'
     merged_df = pd.merge(df1, df2, on=['Employee Name', 'Ticket Date'], how='left')
 
+    # Add 0.5 to 'Actual Hours Worked' column if there is a WTL Start Date and WTL End Date
+    merged_df.loc[
+        ~merged_df['WTL Start Date'].isnull() & ~merged_df['WTL End Date'].isnull(), 'Actual Hours Worked'] += 0.5
+
     # If 'Agency' is blank, fill with 'CSI'
     merged_df['Agency'] = merged_df['Agency'].fillna('CSI')
 
     # Create a new dataframe for rows with errors
-    errors_df = merged_df[(merged_df['Actual Clock In Time'].isna()) |
-                          (merged_df['Actual Clock Out Time'].isna()) |
+    errors_df = merged_df[(merged_df['Clock In'].isna()) |
+                          (merged_df['Clock Out'].isna()) |
                           (merged_df['Actual Hours Worked'] < 8)].copy()
-
 
     # Create the 'Error Description' column
     def generate_error_desc(row):
-        if pd.isnull(row['Actual Clock In Time']) and pd.isnull(row['Actual Clock Out Time']):
+        if pd.isnull(row['Clock In']) and pd.isnull(row['Clock Out']):
             return 'No Clock In or Clock Out Time'
-        elif pd.isnull(row['Actual Clock In Time']):
+        elif pd.isnull(row['Clock In']):
             return 'No Clock In'
-        elif pd.isnull(row['Actual Clock Out Time']):
+        elif pd.isnull(row['Clock Out']):
             return 'No Clock Out'
         elif row['Actual Hours Worked'] < 8:
             return 'Less Than 8 Hours'
@@ -85,19 +85,19 @@ try:
     errors_df['Ticket Date'] = errors_df['Ticket Date'].dt.strftime('%m/%d/%Y')
 
     # Write the dataframes into a new Excel file with two sheets
-    # C:\\Test\\Payroll.xlsx
-    with pd.ExcelWriter('C:\\Users\\tj-fo\\Desktop\\Test\\Results.xlsx') as writer:
+    with pd.ExcelWriter('C:/Users/tj-fo/Desktop/Test/Payroll.xlsx') as writer:
         merged_df.to_excel(writer, sheet_name='Payroll', index=False)
         errors_df.to_excel(writer, sheet_name='Errors', index=False)
 
-    # Load workbook
-    wb = load_workbook('C:\\Users\\tj-fo\\Desktop\\Test\\Payroll.xlsx')  # Update with the correct file path
-    # Read the Excel file
-    df = pd.read_excel('C:\\Users\\tj-fo\\Desktop\\Test\\Payroll.xlsx')
+    # SB
+    # Load the workbook
+    wb = load_workbook('C:/Users/tj-fo/Desktop/Test/Payroll.xlsx')
 
+    # SB
     # Select the sheets
     sheet1 = wb['Payroll']
 
+    # SB
     # Create a red bold font
     red_bold_font = Font(color="FF0000", bold=True)
 
@@ -112,6 +112,7 @@ try:
                     cell.value = 'Clock Out Time?'
                     cell.font = red_bold_font
 
+    # SB
     # Set column widths
     sheet1.column_dimensions['A'].width = 11.26
     sheet1.column_dimensions['B'].width = 26.14
@@ -120,23 +121,21 @@ try:
     sheet1.column_dimensions['E'].width = 20.43
     sheet1.column_dimensions['F'].width = 18.71
     sheet1.column_dimensions['G'].width = 20.86
-    sheet1.column_dimensions['H'].width = 32.57
-    sheet1.column_dimensions['I'].width = 28.71
-    sheet1.column_dimensions['J'].width = 22.86
-    sheet1.column_dimensions['K'].width = 57.86
+    sheet1.column_dimensions['H'].width = 16
+    sheet1.column_dimensions['I'].width = 26
+    sheet1.column_dimensions['J'].width = 19
+    sheet1.column_dimensions['K'].width = 20
+    sheet1.column_dimensions['L'].width = 19
+    sheet1.column_dimensions['M'].width = 29
+    sheet1.column_dimensions['N'].width = 22
+    sheet1.column_dimensions['O'].width = 71.57
+    sheet1.column_dimensions['P'].width = 25
+    sheet1.column_dimensions['Q'].width = 18
+    sheet1.column_dimensions['R'].width = 20.43
+    sheet1.column_dimensions['S'].width = 20.43
 
-    # C:\\Test\\Payroll.xlsx
-    # Save workbook
-    wb.save('C:\\Users\\tj-fo\\Desktop\\Test\\Payroll.xlsx')
-    # Load workbook
-    wb = load_workbook('C:\\Users\\tj-fo\\Desktop\\Test\\Payroll.xlsx')  # Update with the correct file path
-    # Read the Excel file
-    df = pd.read_excel('C:\\Users\\tj-fo\\Desktop\\Test\\Payroll.xlsx')
-    # Select the sheet
-    sheet2 = wb['Payroll']
-
-    # Create a red bold font
-    red_bold_font = Font(color="FF0000", bold=True)
+    # Select the sheets
+    sheet2 = wb['Errors']
 
     # Set column widths
     sheet2.column_dimensions['A'].width = 11.26
@@ -146,10 +145,21 @@ try:
     sheet2.column_dimensions['E'].width = 20.43
     sheet2.column_dimensions['F'].width = 18.71
     sheet2.column_dimensions['G'].width = 20.86
-    sheet2.column_dimensions['H'].width = 32.57
-    sheet2.column_dimensions['I'].width = 28.71
-    sheet2.column_dimensions['J'].width = 22.86
-    sheet2.column_dimensions['K'].width = 57.86
+    sheet2.column_dimensions['H'].width = 18
+    sheet2.column_dimensions['I'].width = 32.57
+    sheet2.column_dimensions['J'].width = 28.71
+    sheet2.column_dimensions['K'].width = 20
+    sheet2.column_dimensions['L'].width = 22.86
+    sheet2.column_dimensions['M'].width = 33.86
+    sheet2.column_dimensions['N'].width = 71.57
+    sheet2.column_dimensions['O'].width = 31.86
+    sheet2.column_dimensions['P'].width = 20.43
+    sheet2.column_dimensions['Q'].width = 20.43
+    sheet2.column_dimensions['R'].width = 20.43
+
+    # Save workbook
+    wb.save('C:/Users/tj-fo/Desktop/Test/Payroll.xlsx')
+
 
     def round_time(dt):
         # Calculate the number of minutes past the last 15-minute mark
@@ -163,27 +173,48 @@ try:
 
         return dt
 
-    # Convert the 'Ticket Date', 'Actual Clock In Time', and 'Actual Clock Out Time' columns to datetime
+
+    # Load the Excel file
+    df = pd.read_excel('C:/Users/tj-fo/Desktop/Test/Payroll.xlsx')
+
+    # Load the Employee Name data from Test3.xlsx file
+    df_test3 = pd.read_excel('C:/Users/tj-fo/Desktop/Test/Test3.xlsx')
+    employee_names_test3 = df_test3['Employee Name'].unique()
+
+    # Convert the 'Ticket Date', 'Clock In', and 'Clock Out' columns to datetime
     df['Ticket Date'] = pd.to_datetime(df['Ticket Date'])
-    df['Actual Clock In Time'] = pd.to_datetime(df['Actual Clock In Time'])
-    df['Actual Clock Out Time'] = pd.to_datetime(df['Actual Clock Out Time'])
+    df['Clock In'] = pd.to_datetime(df['Clock In']).apply(round_time)
+    df['Clock Out'] = pd.to_datetime(df['Clock Out']).apply(round_time)
 
     # Calculate the total hours worked for each job
-    df['Total Hours Worked'] = (df['Actual Clock Out Time'] - df['Actual Clock In Time']).dt.total_seconds() / 3600
+    df['Total Hours Worked'] = (df['Clock Out'] - df['Clock In']).dt.total_seconds() / 3600
 
     # Create a list to hold the results
     results = []
 
-    # Group by 'Employee Name', 'Quote/Job Number Number', 'Agency', and 'Ticket Date'
-    grouped = df.groupby(['Employee Name', 'Quote/Job Number Number', 'Agency', df['Ticket Date'].dt.date])
+    # Create a DataFrame to hold missing Employees
+    missing_employees = pd.DataFrame(columns=['Employee Name', 'Ticket Date'])
+
+    # Group by 'Employee Name', 'JobNo|Customer|Description', 'Agency', and 'Ticket Date'
+    grouped = df.groupby(['Employee Name', 'JobNo|Customer|Description', 'Agency', df['Ticket Date'].dt.date])
+
+    for name, group in grouped:
+        if name[0] not in employee_names_test3:
+            missing_employees = missing_employees.append({'Employee Name': name[0], 'Ticket Date': name[3]},
+                                                         ignore_index=True)
 
     for name, group in grouped:
         total_hours = group['Total Hours Worked'].sum()
-        regular_hours = round(min(8, total_hours), 2)
-        overtime_hours = round(max(0, total_hours - 8), 2)
+
         # Deduct 30 minutes for lunch break if the employee worked for more than 5 hours
         if total_hours > 5:
             total_hours -= 0.5
+
+        regular_hours = round(min(8, total_hours), 2)
+        overtime_hours = round(max(0, total_hours - 8), 2)
+
+        # SB
+        print(group.columns)
 
         # If the Ticket Date is on a Saturday or Sunday, all hours are overtime
         if group['Ticket Date'].dt.dayofweek.iloc[0] >= 5:
@@ -192,38 +223,51 @@ try:
 
         results.append(pd.DataFrame({
             'Employee Name': [name[0]],
+            'Employee ID': [group['Employee ID'].iloc[0]],
             'JobNo|Customer|Description': [name[1]],
             'Agency': [name[2]],
-            'Ticket Date': [group['Ticket Date'].dt.date.iloc[0]],
+            'Ticket Date': [name[3]],
             'Day': [group['Ticket Date'].dt.day_name().iloc[0]],
             'Regular Hours': [regular_hours],
             'Overtime Hours': [overtime_hours],
-            'Actual Clock In Time': [group['Actual Clock In Time'].iloc[0]],
-            'Actual Clock Out Time': [group['Actual Clock Out Time'].iloc[0]]
-            # 'Supervisors Name': [supervisors_name]
+            'Clock In': [group['Clock In'].iloc[0]],
+            'Clock Out': [group['Clock Out'].iloc[0]],
+            'Supervisors Name': [group['Supervisors Name'].iloc[0]],
+            'PM Assigned': [group['PM Assigned'].iloc[0]],
+            'Email': [group['Email'].iloc[0]],
+            'WTL Approved': [group['WTL Approved'].iloc[0]]
         }))
 
     # Concatenate all the results into a single dataframe
     result = pd.concat(results)
 
-    # # Open the save file dialog
-    # file_path = asksaveasfilename()
-
-    # # Check if a file path was selected
-    # if file_path:
-    #     # Open the file and write some content
-    #     with open(file_path, 'w') as file:
-    #         file.write('This is some example content.')
-    #
-    #     print(f"File saved at: {file_path}")
-    # else:
-    #     print("No file selected.")
-
-    # C:\\Test\\Payroll.xlsx
     # Save the result to a new Excel file
-    result.to_excel('C:\\Users\\tj-fo\\Desktop\\Test\\Payroll.xlsx', index=False)
-    # Destroy the Tkinter root window
-    root.destroy()
+    result.to_excel('C:/Users/tj-fo/Desktop/Test/Results.xlsx', index=False)
+
+    # Load the workbook
+    book = load_workbook('C:/Users/tj-fo/Desktop/Test/Results.xlsx')
+
+    # Access the sheet by name or index
+    sheet1 = book['Sheet1']
+
+    # Set column widths
+    sheet1.column_dimensions['A'].width = 29.71
+    sheet1.column_dimensions['B'].width = 12.57
+    sheet1.column_dimensions['C'].width = 72.71
+    sheet1.column_dimensions['D'].width = 17
+    sheet1.column_dimensions['E'].width = 14.29
+    sheet1.column_dimensions['F'].width = 15.14
+    sheet1.column_dimensions['G'].width = 15
+    sheet1.column_dimensions['H'].width = 16.71
+    sheet1.column_dimensions['I'].width = 20.29
+    sheet1.column_dimensions['J'].width = 27.71
+    sheet1.column_dimensions['K'].width = 22.43
+    sheet1.column_dimensions['L'].width = 23.57
+    sheet1.column_dimensions['M'].width = 35.29
+    sheet1.column_dimensions['N'].width = 20.57
+
+    # Save the modified workbook
+    book.save('C:/Users/tj-fo/Desktop/Test/Results.xlsx')
 except Exception as e:
     print("An error occurred:", str(e))
     raise SystemExit
